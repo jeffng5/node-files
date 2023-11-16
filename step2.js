97cd24a36b92157610e84aaab871449142f6f67e
jeffng5 <jeffreykng@hotmail.com>
2023-11-16 17:31:08 -0500

    -added node.js assignments

---
+++ step2.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const process = require('process')
+const axios = require('axios')
+
+
+function cat(path){
+
+fs.readFile(path, 'utf8', (err, data) => {
+if (err){
+    console.log('ERROR=:', err)
+    process.exit(1)
+}
+else {console.log('data:', data)
+}}
+)};
+
+
+
+
+async function webCat(url){
+const data = await axios.get(url)
+console.log(data)
+
+}
+
+let urls = process.argv[2]
+
+
+
+if (urls.slice(0,4)==='http'){
+    webCat(urls)}
+else {
+    cat(urls)}
+;
+
